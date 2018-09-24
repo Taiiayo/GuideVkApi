09d91b1e8c57c3012856dd449fb416d4db0871e9
Taiiayo <taiiakinyo@gmail.com>
2018-09-24 15:40:45 +0300

    first commit and first try to use api

---
+++ GuideVkApi/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace GuideVkApi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.logo = new System.Windows.Forms.Label();
+            this.authorisationButton = new System.Windows.Forms.Button();
+            this.emailtextBox = new System.Windows.Forms.TextBox();
+            this.passTextBox = new System.Windows.Forms.TextBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.friendsListBox = new System.Windows.Forms.ListBox();
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.historyListBox = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // logo
+            // 
+            this.logo.AutoSize = true;
+            this.logo.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.logo.Location = new System.Drawing.Point(12, 9);
+            this.logo.Name = "logo";
+            this.logo.Size = new System.Drawing.Size(180, 33);
+            this.logo.TabIndex = 0;
+            this.logo.Text = "Guide VkNet";
+            // 
+            // authorisationButton
+            // 
+            this.authorisationButton.Location = new System.Drawing.Point(18, 67);
+            this.authorisationButton.Name = "authorisationButton";
+            this.authorisationButton.Size = new System.Drawing.Size(101, 23);
+            this.authorisationButton.TabIndex = 1;
+            this.authorisationButton.Text = "Authorisation";
+            this.authorisationButton.UseVisualStyleBackColor = true;
+            this.authorisationButton.Click += new System.EventHandler(this.authorisationButton_Click);
+            // 
+            // emailtextBox
+            // 
+            this.emailtextBox.Location = new System.Drawing.Point(94, 15);
+            this.emailtextBox.Name = "emailtextBox";
+            this.emailtextBox.Size = new System.Drawing.Size(100, 20);
+            this.emailtextBox.TabIndex = 2;
+            // 
+            // passTextBox
+            // 
+            this.passTextBox.Location = new System.Drawing.Point(94, 41);
+            this.passTextBox.Name = "passTextBox";
+            this.passTextBox.PasswordChar = '*';
+            this.passTextBox.Size = new System.Drawing.Size(100, 20);
+            this.passTextBox.TabIndex = 3;
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(153, 284);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(75, 23);
+            this.sendButton.TabIndex = 4;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // friendsListBox
+            // 
+            this.friendsListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.friendsListBox.FormattingEnabled = true;
+            this.friendsListBox.ItemHeight = 18;
+            this.friendsListBox.Location = new System.Drawing.Point(240, 12);
+            this.friendsListBox.Name = "friendsListBox";
+            this.friendsListBox.Size = new System.Drawing.Size(179, 292);
+            this.friendsListBox.TabIndex = 5;
+            this.friendsListBox.SelectedIndexChanged += new System.EventHandler(this.friendsListBox_SelectedIndexChanged);
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.Location = new System.Drawing.Point(18, 170);
+            this.messageTextBox.Multiline = true;
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.Size = new System.Drawing.Size(210, 108);
+            this.messageTextBox.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "login:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "password:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 319);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Message history";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.emailtextBox);
+            this.groupBox1.Controls.Add(this.passTextBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.authorisationButton);
+            this.groupBox1.Location = new System.Drawing.Point(18, 45);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(210, 100);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            // 
+            // historyListBox
+            // 
+            this.historyListBox.FormattingEnabled = true;
+            this.historyListBox.Location = new System.Drawing.Point(18, 335);
+            this.historyListBox.Name = "historyListBox";
+            this.historyListBox.Size = new System.Drawing.Size(401, 108);
+            this.historyListBox.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 461);
+            this.Controls.Add(this.historyListBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.messageTextBox);
+            this.Controls.Add(this.friendsListBox);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.logo);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Guide VkNet";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label logo;
+        private System.Windows.Forms.Button authorisationButton;
+        private System.Windows.Forms.TextBox emailtextBox;
+        private System.Windows.Forms.TextBox passTextBox;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.ListBox friendsListBox;
+        private System.Windows.Forms.TextBox messageTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox historyListBox;
+    }
+}
+
---
+++ GuideVkApi/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Windows.Forms;
+using VkNet;
+using VkNet.Enums.Filters;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
+
+namespace GuideVkApi
+{                                                                                                  //Хочу сделать классы, возможно, но главное - хочу, чтобы имя отправителя отображалось, как имя
+                                                                                                      // а не как ID.
+    public partial class Form1 : Form
+    {
+        VkApi vkApi = new VkApi();
+        private long userMessageID = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void authorisationButton_Click(object sender, EventArgs e)
+        {
+            ulong appID = 6651879;
+            string email = emailtextBox.Text;
+            string password = passTextBox.Text;
+            Settings settings = Settings.All;
+
+            ApiAuthParams authParams = new ApiAuthParams()
+            {
+                ApplicationId = appID,
+                Settings = settings,
+                Password = password,
+                Login = email
+            };
+
+            try
+            {
+                friendsListBox.Items.Clear();
+                vkApi.Authorize(authParams);
+                GetFrindList();
+            }
+            catch
+            {
+                MessageBox.Show(
+                    "Something went wrong. Check your password and login. " +
+                    "You may also check your internet connection.",
+                    "Error");
+            }
+            
+        }
+
+        public void GetFrindList()
+        {
+            ProfileFields profileFields = ProfileFields.FirstName | ProfileFields.LastName | ProfileFields.Uid;
+            var users = vkApi.Friends.Get(new FriendsGetParams { UserId = vkApi.UserId, Fields = profileFields });
+            foreach (var friends in users)
+            {
+                friendsListBox.Items.Add(friends.FirstName + " " + friends.LastName + " | " + friends.Id);
+            }
+        }
+
+        private void friendsListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            userMessageID = long.Parse(friendsListBox.Text.Substring(friendsListBox.Text.LastIndexOf(' ')));
+            GetMessageHistory();
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            if (messageTextBox.Text != null)
+            {
+                vkApi.Messages.Send(new MessagesSendParams
+                {
+                    UserId = userMessageID,
+                    Message = messageTextBox.Text
+                });
+                messageTextBox.Text = null;
+            }
+        }
+
+        void GetMessageHistory()
+        {
+            historyListBox.Items.Clear();
+            string currenUserName = vkApi.Account.GetProfileInfoAsync().Result.FirstName;
+            var getHistory = vkApi.Messages.GetHistory(new MessagesGetHistoryParams
+            {
+                Count = 15,
+                UserId = long.Parse(userMessageID.ToString())
+            });
+
+            foreach (var message in getHistory.Messages )
+            {
+                if (message.FromId == userMessageID)
+                {
+                    historyListBox.Items.Add(message.Date + " " +  friendsListBox.Text.Substring(0, friendsListBox.Text.IndexOf(' ')) +
+                                             ": " + message.Text + Environment.NewLine);
+                }
+                else
+                {
+                    historyListBox.Items.Add(currenUserName + ": " + message.Text);
+                }
+                
+            }
+        }
+    }
+}
